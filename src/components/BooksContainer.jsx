31a06116fcfab67d8e86eeb9a157cd3d5082cb39
Evgeny Koval <evgenekoval@gmail.com>
2020-03-18 01:23:12 +0700

    Books container added, regenerator-runtime imported

--- src/components/BooksContainer.jsx
+++ src/components/BooksContainer.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { requestBooks } from '../redux/bookShopReducer.js'
 
-export default class BoooksContainer extends Component {
+ class BooksContainer extends Component {
+    componentDidMount(){
+        this.props.requestBooks();
+    }
     render() {
         return (
             <div>
@@ -9,3 +14,12 @@ export default class BoooksContainer extends Component {
         )
     }
 }
+
+const mapStateToProps = state =>{
+    return{
+        books: state.bookShopPage.books,
+        isFetching: state.bookShopPage.isFetching,
+    }
+}
+
+export default connect(mapStateToProps, {requestBooks})(BooksContainer)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MainApp from "./components/App.js";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 ReactDOM.render(<MainApp />, document.getElementById("app"));
