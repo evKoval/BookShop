26e24b79d80060d9c525814ef7fe5574b2310455
Evgeny Koval <evgenekoval@gmail.com>
2020-03-18 13:21:17 +0700

    createSagaMiddleware in store

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,13 +1,16 @@
 import { combineReducers, compose, applyMiddleware, createStore } from "redux";
 import bookShopReducer from "./bookShopReducer";
-import thunkMiddleware from "redux-thunk";
+// import thunkMiddleware from "redux-thunk";
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 
 let reducers = combineReducers({
     bookShopPage: bookShopReducer
 })
 
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(logger, sagaMiddleware)));
 
 window._store_ = store;
 
